821ed1ce789cd3672c40d7b1c457b89dff98349a
Jabo Arnold Landry <arnoldjabo@gmail.com>
2025-12-22 18:34:12 +0200

    feat:Add dynamic links
    this changes will help the link to be more dynamic and reused across the project, before we had static links and now I changed it to behave more dynamic
    
    I create an object to hold key value pairs about the links details, the links are keys and the url is the value for a given a key and then loop through them at the same time populating them to the DOM which will help in future for scalability of the project and reduces number of codes we have to go through in HTML page which reduces traffic

--- src/Javascript/index.js
+++ src/Javascript/index.js
@@ -1,1 +1,25 @@
-console.log("Space ship Tourism");
+const navLinkSection = document.querySelector("#nav-links");
+
+const linkList = [
+  { link: "00 home", url: "#" },
+  { link: "01 destination", url: "#" },
+  { link: "02 crew", url: "#" },
+  { link: "03 technology", url: "#" },
+];
+
+navLinkSection.textContent = "";
+
+linkList.forEach((links) => {
+  const { link, url } = links;
+  navLinkSection.innerHTML += `<a
+                                    href=${url}
+                                    class="uppercase font-extralight text-sm flex gap-2 relative py-2 group"
+                                    >
+                                    <span>${link.substring(0, 3)}</span> 
+                                    ${link.substring(3)}
+                                    <span
+                                        class="absolute top-14 left-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"
+                                    >
+                                    </span>
+                                </a>`;
+});
