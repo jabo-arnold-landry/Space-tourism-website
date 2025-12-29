3ed5b02d2fa6063c4f174457bd871bc41a561506
Jabo Arnold Landry <arnoldjabo@gmail.com>
2025-12-29 10:54:11 +0200

    feat:Add destination js file to handle its operation
    this will help to make a modular and easy to maintain and navigate
    
    this file holds DOM manupilation of the destination section with the imports that are common in all files I will be creating
    
    those common functions include the fetch function to get the data files for a given section and the function to show the content based on where the user left off the last time they visited the site

---
+++ src/Javascript/destinationJSFile.js
@@ -0,0 +1,104 @@
+const destinationSwitcherBtns = document.querySelector("#destination-switcher");
+const destinationDetailsSection = document.querySelector("#dest-details");
+const imgDestination = document.querySelector("#dest-img");
+
+import { dataSet, itemdINdex } from "./index.js";
+const destinationList = ["moon", "mars", "europa", "titian"];
+
+function destinationSwitcherBtnsCreation() {
+  destinationSwitcherBtns.textContent = "";
+  const btnDocFragment = document.createDocumentFragment();
+
+  destinationList.forEach((destination, index) => {
+    const btn = document.createElement("button");
+
+    btn.classList.add(
+      "text-gray-400",
+      "uppercase",
+      "font-light",
+      "text-sm",
+      "cursor-pointer",
+      "group",
+      "relative"
+    );
+    btn.setAttribute("data-index", index);
+
+    btn.innerHTML = `${destination}
+                      <span
+                        class="absolute top-6 left-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"
+                      ></span>`;
+
+    btnDocFragment.append(btn);
+  });
+
+  destinationSwitcherBtns.append(btnDocFragment);
+}
+destinationSwitcherBtnsCreation();
+
+function displayDestanationDetails(dataSet, index) {
+  const { destinations } = dataSet;
+  const destanationData = destinations[index];
+
+  highlightingCurrentBtn(index);
+
+  localStorage.setItem("destinations", JSON.stringify({ destinations, index }));
+
+  destinationDetailsSection.innerHTML = "";
+  const {
+    name,
+    images: { webp },
+    description,
+    distance,
+    travel,
+  } = destanationData;
+
+  imgDestination.src = webp;
+  destinationDetailsSection.innerHTML = `<div class="grid gap-5">
+              <strong class="text-6xl font-extralight uppercase">${name}</strong>
+              <p class="max-w-100 text-gray-400 pb-4">
+               ${description}
+              </p>
+            </div>
+
+            <div class="pt-4 flex justify-between px-4">
+              <div>
+                <p class="text-xs mb-2 text-gray-500 uppercase">
+                  avg. distance
+                </p>
+                <p class="text-4xl text-gray-200 font-extralight">${distance}</p>
+              </div>
+
+              <div>
+                <p class="text-xs mb-2 text-gray-500 uppercase">
+                  est. travel time
+                </p>
+                <p class="text-4xl text-gray-200 font-extralight">${travel}</p>
+              </div>
+            </div>`;
+}
+
+const destinationIndex = itemdINdex("destinations");
+
+displayDestanationDetails(dataSet, destinationIndex);
+
+//event listeners section
+
+destinationSwitcherBtns.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const id = e.target.dataset.index;
+
+    displayDestanationDetails(dataSet, id);
+  }
+});
+
+function highlightingCurrentBtn(index) {
+  const btns = document.querySelectorAll("button");
+
+  const spans = destinationSwitcherBtns.querySelectorAll("span");
+
+  spans.forEach((span) => span.classList.add("scale-x-0"));
+
+  const selectedBtnSpan = btns[index].querySelector("span");
+
+  selectedBtnSpan.classList.remove("scale-x-0");
+}
