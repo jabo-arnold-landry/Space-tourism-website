0fef15d2475908098cbecc46ae88898819d19b19
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-06 15:11:06 +0200

    fix:Add session storage for data storage instead of local storage

--- src/Javascript/utilis.js
+++ src/Javascript/utilis.js
@@ -1,9 +1,9 @@
 export default function saveDataToBrowserMemory(itemName, itemData, index) {
-  return localStorage.setItem(itemName, JSON.stringify({ itemData, index }));
+  return sessionStorage.setItem(itemName, JSON.stringify({ itemData, index }));
 }
 
 export function itemdINdex(itemName) {
-  const destinationData = localStorage.getItem(itemName);
+  const destinationData = sessionStorage.getItem(itemName);
   
   //get current index to display last visited page in localstorage
   return destinationData ? JSON.parse(destinationData)["index"] : 0;
